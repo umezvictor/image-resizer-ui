975b3e1d8e12ce08a44da66144a207d954eeae75
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2026-02-21 07:39:30 +0200

    added signalr url

--- src/components/ImageUpload/ImageUploadForm.tsx
+++ src/components/ImageUpload/ImageUploadForm.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import "./ImageUpload.css";
+import { useEffect, useRef } from "react";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 const fileSchema = z.object({
   file: z
@@ -21,6 +23,8 @@ function ImageUploadForm() {
   } = useForm<CompressFileRequest>({
     resolver: zodResolver(fileSchema),
   });
+
+  const connectionRef = useRef<HubConnection | null>(null);
 
   const submitHandler = async (data: CompressFileRequest) => {
     try {
@@ -42,6 +46,54 @@ function ImageUploadForm() {
       console.error("Error uploading file:", error);
     }
   };
+
+  const SIGNALR_NEGOTIATE_URL =
+    "https://imageresizer-h8cjegbuafcvh6c0.westeurope-01.azurewebsites.net/negotiate";
+
+  //SignalR connection and event handlers
+  useEffect(() => {
+    const initConnection = async () => {
+      try {
+        // 1. Call negotiate endpoint
+        const res = await fetch(SIGNALR_NEGOTIATE_URL, { method: "POST" });
+        console.log("res");
+        console.log(res);
+        const info = await res.json();
+        console.log("info");
+        console.log(info);
+        // info.url and info.accessToken are provided by the function
+
+        // 2. Create SignalR connection using negotiated details
+        const connection = new HubConnectionBuilder()
+          .withUrl(info.url, {
+            accessTokenFactory: () => info.accessToken,
+          })
+          .withAutomaticReconnect([0, 2000, 5000, 10000, 15000])
+          .build();
+
+        // 3. Start the connection
+        await connection.start();
+        console.log("Connection successful");
+        connectionRef.current = connection;
+
+        // 4. (Optional) Set up a handler for your SignalR events
+        connection.on("resized-image", (downloadUrl) => {
+          console.log("New image:", downloadUrl);
+          // Handle the new image URL (e.g., update state or display UI)
+        });
+      } catch (error) {
+        console.error("SignalR connection failed:", error);
+      }
+    };
+
+    initConnection();
+
+    return () => {
+      if (connectionRef.current) {
+        connectionRef.current.stop();
+      }
+    };
+  }, []);
 
   return (
     <>
